7e19ad456d21f7da711e82096e032d6bc069c7ef
SPNTWALA <79240302+SPNTWALA@users.noreply.github.com>
2023-03-26 18:08:07 +0500

    second

--- js/script.js
+++ js/script.js
@@ -1,31 +1,34 @@
 // call forms
-
 const btnOpenAuth = document.querySelector('.auth_btn');
 const formAuth = document.querySelector('.formAuth');
-btnOpenAuth.addEventListener('click', ()=>{
-    formAuth.classList.add('formAuth_open');
-});
-
+if (btnOpenAuth) {
+    btnOpenAuth.addEventListener('click', ()=>{
+        formAuth.classList.add('formAuth_open');
+    }, true);
+};
 let spanAuth = document.getElementsByClassName("closeAuth")[0];
-spanAuth.onclick = function() {
-    formAuth.classList.remove('formAuth_open');
-}
+if (spanAuth) {
+    spanAuth.addEventListener('click', () => {
+        formAuth.classList.remove('formAuth_open');
+    }, true);
+};
 
 const btnOpenReg = document.querySelector('.reg_btn');
 const formReg = document.querySelector('.formReg');
-btnOpenReg.addEventListener('click', ()=>{
-    formReg.classList.add('formReg_open');
-});
-
+if (btnOpenReg) {
+    btnOpenReg.addEventListener('click', ()=>{
+        formReg.classList.add('formReg_open');
+    }, true);
+};
 let spanReg = document.getElementsByClassName("closeReg")[0];
-spanReg.onclick = function() {
-    formReg.classList.remove('formReg_open');
-}
+if (spanReg) {
+    spanReg.addEventListener('click', () => {
+        formReg.classList.remove('formReg_open');
+    }, true);
+};
 
 //accordeon
-const stage = document.querySelector(".stage_item");
-const stageTitles = stage.querySelectorAll(".stg_item");
-
+const stageTitles = document.querySelectorAll(".stg_item");
 stageTitles.forEach.call(stageTitles, function (stageTitle) {
     stageTitle.addEventListener("click", function () {
         const clickedStage = stageTitle.parentElement.querySelector(".stage_txt");
@@ -44,11 +47,9 @@ stageTitles.forEach.call(stageTitles, function (stageTitle) {
 const olympBlock = document.querySelectorAll('.wrapper_block');
 const rightBtn = document.querySelectorAll('.olymp_arrow-right');
 const leftBtn = document.querySelectorAll('.olymp_arrow-left');
-
 olympBlock.forEach((item, i) => {
     let blockDemisions = item.getBoundingClientRect();
     let blockWidth = blockDemisions.width;
-
     rightBtn[i].addEventListener('click', () => {
         item.scrollLeft += blockWidth/2;
     });
@@ -59,9 +60,57 @@ olympBlock.forEach((item, i) => {
 
 const nav = document.getElementById("navbar-toggler");
 const menus = document.querySelectorAll(".formAuth");
+if (nav) {
+    nav.addEventListener('click', () => {
+        for (let el of menus) {
+            el.classList.toggle("formAuth_open");
+        };
+    }, true);
+};
 
-nav.addEventListener('click', () => {
-    for (let el of menus) {
-        el.classList.toggle("formAuth_open");
-    };
-});
+//menu
+const navbarBtn = document.getElementById("navbar");
+const menu = document.querySelectorAll(".header_btn-profile");
+if (navbarBtn) {
+    navbarBtn.addEventListener('click', () => {
+        for (let el of menus) {
+            el.classList.toggle("show");
+        };
+    }, true);
+};
+
+//send
+const btnSend = document.querySelector('.btn_send');
+const modalSend = document.querySelector('.sendModal');
+if (btnSend) {
+    btnSend.addEventListener('click', ()=>{
+        modalSend.classList.add('sendModal_open');
+    }, true);
+};
+let spanSend = document.getElementsByClassName("closeSend")[0];
+if (spanSend) {
+    spanSend.addEventListener('click', () => {
+        modalSend.classList.remove('sendModal_open');
+    }, true);
+};
+let spanCan = document.getElementsByClassName("btn_cancel")[0];
+if (spanCan) {
+    spanCan.addEventListener('click', () => {
+        modalSend.classList.remove('sendModal_open');
+    }, true);
+};
+
+const btnAgree = document.querySelector('.btn_agree');
+const modalNotice = document.querySelector('.noticeModal');
+if (btnAgree) {
+    btnAgree.addEventListener('click', ()=>{
+        modalNotice.classList.add('noticeModal_open');
+        modalSend.classList.remove('sendModal_open');
+    }, true);
+};
+let spanNotice = document.getElementsByClassName("closeNotice")[0];
+if (spanNotice) {
+    spanNotice.addEventListener('click', () => {
+        modalNotice.classList.remove('noticeModal_open');
+    }, true);
+};
